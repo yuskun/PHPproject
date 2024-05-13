a2425b14771c86894504847dbc15e7c97b203819
yuskun <yoyoppp930710@gmail.com>
2024-05-13 14:42:40 +0800

    courseAndmessage Done

---
+++ php/personal.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  include "../html/nav.html";
+  ?>
+  <?php
+    include "../html/personal.html";
+  ?> 
+    
+  <?php
+    include "../html/footer.html";
+  ?>
+
+
+</body>
+
+</html>
